d73aa51c600c35eccdf51223742fb9ed177d14bf
RainyKnight <stoltmanlucas@gmail.com>
2021-11-20 10:10:25 -0800

    Finalize 1929.h

--- 1929.h
+++ 1929.h
@@ -2,14 +2,33 @@
 
 #include <iostream>
 
+// begin solution
+std::vector<int> getConcatenation(std::vector<int>& nums) {
+   std::vector<int> result;
 
-   vector<int> getConcatenation(vector<int>& nums) {
-        vector<int> result;
-    
-        for (int u = 0; u < 2; u++){
-            for (int i = 0; i < nums.size(); i++){
-                result.push_back(nums[i]);
-            }
-        }
-        return result;
-    }
+   for (int u = 0; u < 2; u++) {
+      for (int i = 0; i < nums.size(); i++) {
+         result.push_back(nums[i]);
+      }
+   }
+   return result;
+}
+// end solution
+
+
+void run1929() {
+   std::cout << "1929: Concatenation of Array" << std::endl;
+   std::vector<int> vect{2, 5, 2, 6, 3, 4, 6};
+
+   std::cout << "Before:\t";
+   for (int x : vect) {
+      std::cout << x << " ";
+   }
+   std::cout << std::endl;
+
+   std::cout << "After:\t";
+   for (int x : getConcatenation(vect)) {
+      std::cout << x << " ";
+   }
+   std::cout << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 
 #include "1480.h"
+#include "1929.h"
 
 using namespace std;
 
 int main() {
-   run1480();
+   // run1480();
+   // run1929();
 }
