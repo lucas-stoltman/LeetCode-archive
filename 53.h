2146639fdd4bbbed3a27d736656d39161df84271
RainyKnight <stoltmanlucas@gmail.com>
2021-11-25 23:43:16 -0800

    Add more boilerplate

--- 53.h
+++ 53.h
@@ -1,2 +1,29 @@
 // https://leetcode.com/problems/maximum-subarray/
 #pragma once
+
+#include <iostream>
+#include <vector>
+
+// begin solution
+int maxSubArray(std::vector<int>& nums) {
+       return -1; 
+    }
+
+// end solution
+
+void run53() {
+   std::cout << "53: Maximum Subarray" << std::endl;
+   std::vector<int> vect{2, 5, 2, 6, 3, 4, 6};
+
+   std::cout << "Before:\t";
+   for (int x : vect) {
+      std::cout << x << " ";
+   }
+   std::cout << std::endl;
+
+   std::cout << "After:\t";
+   // for (int x : maxSubArray(vect)) {
+   //    std::cout << x << " ";
+   // }
+   std::cout << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 // Created by Lucas Stoltman November 19th 2021
 #include <iostream>
 
-#include "53.h"
-#include "540.h"
 #include "1480.h"
 #include "1929.h"
+#include "53.h"
+#include "540.h"
 
 using namespace std;
 
 int main() {
+   run53();
    // run540();
    // run1480();
    // run1929();
