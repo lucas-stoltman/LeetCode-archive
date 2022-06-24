be0b8fc813b9c40f892308bf4fe1be8544a1ab92
RainyKnight <stoltmanlucas@gmail.com>
2022-06-23 18:27:23 -0700

    Add basic solution

--- python/724_Find_pivot.py
+++ python/724_Find_pivot.py
@@ -1,1 +1,9 @@
-# TODO add code snippet
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        nums_sum = sum(nums)
+        left_sum = 0
+        for i, x in enumerate(nums):
+            if left_sum == (nums_sum - left_sum - x):
+                return i
+            left_sum += x
+        return -1
