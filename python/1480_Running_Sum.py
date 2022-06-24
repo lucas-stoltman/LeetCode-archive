86be24b58b356e27a194115f3d8b349fda1f96b2
RainyKnight <stoltmanlucas@gmail.com>
2022-06-23 18:27:47 -0700

    Add basic solution

--- python/1480_Running_Sum.py
+++ python/1480_Running_Sum.py
@@ -1,1 +1,9 @@
-# TODO add code snippet
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        result = [nums[0]]
+
+        for i in range(len(nums)):
+            if i > 0:
+                new_sum = result[i - 1] + nums[i]
+                result.append(new_sum)
+        return result
