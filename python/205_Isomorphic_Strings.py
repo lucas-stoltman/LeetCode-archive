f35abe1d1619d6127202537b7f96c47eae8d3e6f
RainyKnight <stoltmanlucas@gmail.com>
2022-06-25 17:11:03 -0700

    Create a helper method

--- python/205_Isomorphic_Strings.py
+++ python/205_Isomorphic_Strings.py
@@ -1,21 +1,33 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         # if the characters map 1:1 then it is isomorphic
-        current = 0
-        first_word_numbers = [current]
-        first_word_letters = [s[0]]
-        i = 1
-        # handles 1 through (len-1)
-        for i in range(len(s)-1):
-            # if the next letter is different
-            if s[i] != s[i+1]:
-                # if the letter is in the list already
-                if s[i] in first_word_letters:
-                    first_word_numbers.append(first_word_letters[i])
-            else:
-                current += 1
-                first_word_letters.append(s[i])
-                first_word_numbers.append(current)
+        def convert(word: str):
+            current = 0
+            word_numbers = []
+            word_letters = []
+            # handles 0 through (len-1)
+            for i in range(len(s)-1):
+                # if the next letter is different
+                if s[i] != s[i+1]:
+                    # if the letter is in the list already
+                    if s[i+1] in word_letters:
+                        # place the same index as the letter
+                        index = word_letters.index(s[i+1])
+                        word_numbers.append(index)
+                    else:
+                        # place the same index as the letter
+                        current += 1
+                        word_letters.append(s[i])
+                        word_numbers.append(current)
+                else:
+                    current += 1
+                    word_letters.append(s[i])
+                    word_numbers.append(current)
+
+            return word_numbers
+
+        first_word = convert(s)
+        second_word = convert(t)
 
         # compare strings of numbers
         if first_word == second_word:
@@ -25,6 +37,6 @@ class Solution:
 
 
 sol = Solution()
-print(sol.isIsomorphic("foodsafjbsad", "babasdf"))
+print(sol.isIsomorphic("cat", ""))
 
 
