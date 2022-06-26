a624b51645bf554c33d8f98d8e2a656ff3131753
RainyKnight <stoltmanlucas@gmail.com>
2022-06-25 17:34:43 -0700

    It's working!

--- python/205_Isomorphic_Strings.py
+++ python/205_Isomorphic_Strings.py
@@ -2,27 +2,31 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         # if the characters map 1:1 then it is isomorphic
         def convert(word: str):
+            # beginning edge case
             current = 0
-            word_numbers = []
-            word_letters = []
-            # handles 0 through (len-1)
-            for i in range(len(s)-1):
-                # if the next letter is different
-                if s[i] != s[i+1]:
+            word_numbers = [current]
+            word_letters = [word[0]]
+
+            # handles 1 through (len-1)
+            for i in range(1, len(word)):
+                # if the current letter is different from the last
+                if word[i] != word[i-1]:
                     # if the letter is in the list already
-                    if s[i+1] in word_letters:
+                    if word[i] in word_letters:
                         # place the same index as the letter
-                        index = word_letters.index(s[i+1])
+                        index = word_letters.index(word[i])
                         word_numbers.append(index)
                     else:
                         # place the same index as the letter
                         current += 1
-                        word_letters.append(s[i])
+                        word_letters.append(word[i])
                         word_numbers.append(current)
                 else:
-                    current += 1
-                    word_letters.append(s[i])
                     word_numbers.append(current)
+
+            # ending edge case
+            # word_numbers = [current]
+            # word_letters = [word[len(word)-1]]
 
             return word_numbers
 
@@ -36,7 +40,8 @@ class Solution:
             return False
 
 
+# ------------------------------------------------
 sol = Solution()
-print(sol.isIsomorphic("cat", ""))
+print(sol.isIsomorphic("cat", "tac"))
 
 
