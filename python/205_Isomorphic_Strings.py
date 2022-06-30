d741dc34d4be2ed51d6c9d36ab23cc57807e7e03
RainyKnight <stoltmanlucas@gmail.com>
2022-06-29 23:27:47 -0700

    Change how the old solution is commented out

--- python/205_Isomorphic_Strings.py
+++ python/205_Isomorphic_Strings.py
@@ -20,43 +20,41 @@ class Solution:
 
         # -------------------------------------------------------
 
-        # ----------------- Old Solution --------------------
-        '''
-        def convert(word: str):
-            # beginning edge case
-            current = 0
-            word_numbers = [current]
-            stored_letters = [word[0]]
-
-            # handles 1 through (len-1)
-            for i in range(1, len(word)):
-                current_letter = word[i]
-                last_letter = word[i-1]
-
-                # if the current letter is different from the last
-                if current_letter not in last_letter:
-                    # if the letter is in the list already
-                    if current_letter in stored_letters:
-                        index = stored_letters.index(current_letter)
-                        # place the same index as the letter
-                        word_numbers.append(index)
-                    else:
-                        # place the same index as the letter
-                        current += 1
-                        stored_letters.append(word[i])
-                        word_numbers.append(current)
-                else:
-                    index = stored_letters.index(current_letter)
-                    word_numbers.append(index)
-            return word_numbers
-
-        first_word = convert(s)
-        second_word = convert(t)
-
-        # compare strings of numbers
-        return first_word == second_word
-        '''
-    # -------------------------------------------------------
+    #     # ----------------- Old Solution --------------------
+    #     def convert(word: str):
+    #         # beginning edge case
+    #         current = 0
+    #         word_numbers = [current]
+    #         stored_letters = [word[0]]
+    #
+    #         # handles 1 through (len-1)
+    #         for i in range(1, len(word)):
+    #             current_letter = word[i]
+    #             last_letter = word[i-1]
+    #
+    #             # if the current letter is different from the last
+    #             if current_letter not in last_letter:
+    #                 # if the letter is in the list already
+    #                 if current_letter in stored_letters:
+    #                     index = stored_letters.index(current_letter)
+    #                     # place the same index as the letter
+    #                     word_numbers.append(index)
+    #                 else:
+    #                     # place the same index as the letter
+    #                     current += 1
+    #                     stored_letters.append(word[i])
+    #                     word_numbers.append(current)
+    #             else:
+    #                 index = stored_letters.index(current_letter)
+    #                 word_numbers.append(index)
+    #         return word_numbers
+    #
+    #     first_word = convert(s)
+    #     second_word = convert(t)
+    #
+    #     # compare strings of numbers
+    #     return first_word == second_word
+    # # -------------------------------------------------------
 
 
 # ------------------------------------------------
