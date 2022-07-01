94d2af6f2cd076b315a09b980cc1c0fb550604be
RainyKnight <stoltmanlucas@gmail.com>
2022-07-01 16:32:43 -0700

    Add a temporary solution that does not work yet

--- python/392_Is_Subsequence.py
+++ python/392_Is_Subsequence.py
@@ -1,1 +1,26 @@
 # TODO solution
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        # search for each letter of s within t
+        # assign the index to the letter in a dictionary
+        # resume searching from that index spot
+
+        word_dict = {}
+
+        # check each letter of target string
+        for i in range(len(s)):
+            # check each letter of input string
+            for j in range(len(t)):
+                # check if the letter is found
+                if t[j] == s[i]:
+                    word_dict[t[j]] = j
+                    print(t[j], j)
+            # if letter is not found
+            return False
+        return True
+
+# ------------------------------------------------
+
+
+sol = Solution()
+print(sol.isSubsequence("abc", "ahbgdc"))
