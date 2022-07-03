c5a2462d81c3b4e0667e2265d7091d40ab4323af
RainyKnight <stoltmanlucas@gmail.com>
2022-07-03 11:18:33 -0700

    Add assertions for testing

--- python/392_Is_Subsequence.py
+++ python/392_Is_Subsequence.py
@@ -2,7 +2,7 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         # search for each letter of s within t
         # assign the index to the letter in a dictionary
-        # resume searching from that index spot
+        # resume searching starting from that index spot
 
         word_dict = {}
         temp = ""
@@ -10,6 +10,8 @@ class Solution:
         # base case
         if s == "":
             return True
+        if t == "":
+            return False
 
         # case 1
         # iterate until it finds the matching letter
@@ -19,7 +21,7 @@ class Solution:
                 temp += t[i]
                 break
             # if letter is not found by the end
-            elif i == len(t)-1:
+            elif i == len(t) - 1:
                 return False
 
         # case 2
@@ -28,7 +30,7 @@ class Solution:
         for i in range(1, len(s)):
             # iterate again, starting from the index of the last letter found
             # check each letter of input string
-            for j in range(word_dict.get(t[j]), len(t)):
+            for j in range(word_dict.get(s[j]), len(t)):
                 # check if the letter is found
                 if t[j] == s[i]:
                     word_dict[t[j]] = j
@@ -36,8 +38,9 @@ class Solution:
                     # print(t[j], j)
                     break
                 # if letter is not found by the end
-                elif j == len(t)-1:
+                elif j == len(t) - 1:
                     return False
+            return False
         # print(temp, s)
         # print(word_dict)
         return temp == s
@@ -45,6 +48,9 @@ class Solution:
 
 # ------------------------------------------------
 
-
 sol = Solution()
-print(sol.isSubsequence("abc", "ahbdc"))
+assert (sol.isSubsequence("abc", "ahbgdc")) is True
+assert (sol.isSubsequence("acb", "ahbgdc")) is False
+assert (sol.isSubsequence("aaaaaa", "bbaaaa")) is False
+assert (sol.isSubsequence("", "abc")) is True
+assert (sol.isSubsequence("abc", "")) is False
