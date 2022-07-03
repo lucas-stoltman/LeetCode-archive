c9daca4f450267940afe1cfe773a1b82cc9ce120
RainyKnight <stoltmanlucas@gmail.com>
2022-07-02 23:37:50 -0700

    Brainstorming better solution

--- python/392_Is_Subsequence.py
+++ python/392_Is_Subsequence.py
@@ -1,4 +1,3 @@
-# TODO solution
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         # search for each letter of s within t
@@ -8,21 +7,43 @@ class Solution:
         word_dict = {}
         temp = ""
 
-        # check each letter of target string
-        for i in range(len(s)):
+        # TODO fix "acb"
+
+        # case 1
+        # iterate until it finds the matching letter
+        # if it reaches the end, return False
+        for i in range(len(t)):
+            if t[i] == s[0]:
+                word_dict[t[i]] = i
+                temp += t[i]
+                print(t[i], i)
+            # if letter is not found by the end
+            elif i == len(t):
+                return False
+
+        # case 2
+        # iterate again, starting from the index of the last letter found
+        # if it reaches the end, return False
+        for i in range(1, len(s)):
+            j = 0
             # check each letter of input string
-            for j in range(len(t)):
+            for j in range(word_dict.get(t[j]), len(t)):
                 # check if the letter is found
                 if t[j] == s[i]:
                     word_dict[t[j]] = j
                     temp += t[j]
-                    # print(t[j], j)
-            # if letter is not found
-            # return False
+                    print(t[j], j)
+                    break
+                # if letter is not found by the end
+                elif j == len(t):
+                    return False
+        print(temp, s)
+        print(word_dict)
         return temp == s
+
 
 # ------------------------------------------------
 
 
 sol = Solution()
-print(sol.isSubsequence("abc", "ahbgdc"))
+print(sol.isSubsequence("acb", "ahbgdc"))
