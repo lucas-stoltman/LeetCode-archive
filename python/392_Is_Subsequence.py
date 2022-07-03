648b38216f93b326483c5885f4fefab339a4e7eb
RainyKnight <stoltmanlucas@gmail.com>
2022-07-03 10:57:16 -0700

    Fix out of order case

--- python/392_Is_Subsequence.py
+++ python/392_Is_Subsequence.py
@@ -7,38 +7,37 @@ class Solution:
         word_dict = {}
         temp = ""
 
-        # TODO fix "acb"
+        # TODO add base case
 
         # case 1
         # iterate until it finds the matching letter
-        # if it reaches the end, return False
         for i in range(len(t)):
             if t[i] == s[0]:
                 word_dict[t[i]] = i
                 temp += t[i]
-                print(t[i], i)
+                break
             # if letter is not found by the end
-            elif i == len(t):
+            elif i == len(t)-1:
                 return False
 
         # case 2
-        # iterate again, starting from the index of the last letter found
-        # if it reaches the end, return False
+        # look for the rest of the letters from s
+        j = 0
         for i in range(1, len(s)):
-            j = 0
+            # iterate again, starting from the index of the last letter found
             # check each letter of input string
             for j in range(word_dict.get(t[j]), len(t)):
                 # check if the letter is found
                 if t[j] == s[i]:
                     word_dict[t[j]] = j
                     temp += t[j]
-                    print(t[j], j)
+                    # print(t[j], j)
                     break
                 # if letter is not found by the end
-                elif j == len(t):
+                elif j == len(t)-1:
                     return False
-        print(temp, s)
-        print(word_dict)
+        # print(temp, s)
+        # print(word_dict)
         return temp == s
 
 
@@ -46,4 +45,4 @@ class Solution:
 
 
 sol = Solution()
-print(sol.isSubsequence("acb", "ahbgdc"))
+print(sol.isSubsequence("abc", "ahbdc"))
