3664684a2c3d18d4a05685c506d8b1e30217c7bc
RainyKnight <stoltmanlucas@gmail.com>
2022-07-05 17:49:19 -0700

    Fix drive file

--- python/main.py
+++ python/main.py
@@ -1,3 +1,7 @@
+# This file was created by Lucas Stoltman on July 5th, 2022
 # the driver file for python attempts
 
-import
+import LC_205   # 205. Isomorphic Strings
+import LC_392   # 392. Is Subsequence
+import LC_724   # 724. Find Pivot Index
+import LC_1480  # 1480. Running Sum of 1d Array
