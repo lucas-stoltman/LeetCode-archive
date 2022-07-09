c8fb9f29268d3ed06612da69a01568c7f6bb5fdb
RainyKnight <stoltmanlucas@gmail.com>
2022-07-08 19:09:06 -0700

    Create boilerplate for new leetcode problems

---
+++ python/LC_Boilerplate.py
@@ -0,0 +1,16 @@
+# <LC Title>
+# <LC URL>
+
+class Solution:
+
+# ------------------------------------------------
+def test(self):
+    # sol = Solution()
+    # assert (sol.isSubsequence("abc", "xaxbxcx")) is True
+    # assert (sol.isSubsequence("acb", "abc")) is False
+    # assert (sol.isSubsequence("aaaaaa", "bbaaaa")) is False
+    # assert (sol.isSubsequence("", "abc")) is True
+    # assert (sol.isSubsequence("abc", "")) is False
+    # print("All tests passed.")
+
+
