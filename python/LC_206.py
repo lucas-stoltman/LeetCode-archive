f5fce1cf4d5a48d5462b798bccbfb14669913ae7
RainyKnight <stoltmanlucas@gmail.com>
2022-07-11 16:17:32 -0700

    Initial commit

---
+++ python/LC_206.py
@@ -0,0 +1,23 @@
+# 206. Reverse Linked List
+# https://leetcode.com/problems/reverse-linked-list/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+
+# ------------------------------------------------
+def test(self):
+    # sol = Solution()
+    # assert (sol.isSubsequence("abc", "xaxbxcx")) is True
+    # assert (sol.isSubsequence("acb", "abc")) is False
+    # assert (sol.isSubsequence("aaaaaa", "bbaaaa")) is False
+    # assert (sol.isSubsequence("", "abc")) is True
+    # assert (sol.isSubsequence("abc", "")) is False
+    # print("All tests passed.")
+
+
